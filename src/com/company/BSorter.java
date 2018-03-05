46e174560ab9a231874e1027b92528ae90e1bcf9
tonygorena <tonygorena@yahoo.com>
2018-03-04 22:03:52 -0600

    Everything but doSort and graph

---
+++ src/com/company/BSorter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class BSorter<E extends Comparable<E>> extends Sorter {
+
+
+    public BSorter(E[] array) {
+        super(array);
+    }
+
+    @Override
+    public void doSort() {
+
+    }
+}
---
+++ src/com/company/QSorter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class QSorter<E extends Comparable<E>> extends Sorter {
+
+
+    public QSorter(E[] array) {
+        super(array);
+    }
+
+    @Override
+    public void doSort() {
+
+    }
+}
